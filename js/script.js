6e393bab5bfd584f774aee55c146ed58e55046e6
herepremg@gmail.com <herepremg@gmail.com>
2016-12-08 12:51:34 +0530

    coming soon
    first update. All file are added

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+function enterContent(){
+  var name=document.getElementById('input-name').value;
+  var message="<h4>Hello "+name+" !</h4>";
+
+  doucment.getElementById('result-content').innerHTML=message;
+
+}
